0ee2c0cd33bceca72fec12ea2ba4d9553e27f342
roopmeet13 <137491633+roopmeet13@users.noreply.github.com>
2023-07-09 16:44:56 +0530

    Update noah (1).sol

--- noah (1).sol
+++ noah (1).sol
@@ -1,69 +1,32 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.16;
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
 
+contract Banking{
+    mapping (address => uint) private balances;
+    event Deposit(address indexed owner, uint amount);
+    event Withdraw(address indexed owner, uint amount);
+    event Transfer(address indexed from, address indexed to, uint amount);
 
-
-contract Assessment {
-    address payable public owner;
-    uint256 public balance;
-
-    event Deposit(uint256 amount);
-    event Withdraw(uint256 amount);
-    
-
-
-    constructor(uint initBalance) payable {
-        owner = payable(msg.sender);
-        balance = initBalance;
+    function deposit(address _account, uint _number) public payable{
+        balances[_account] += _number;
+        emit Deposit(_account,_number);
     }
 
-    function getBalance() public view returns(uint256){
-        return balance;
+    function withdraw(address _account, uint _number) public payable{
+        require(balances[_account]>=_number,"You are broke {:( ");
+        balances[_account] -= _number;
+        emit Withdraw(_account,_number);
     }
 
-    function deposit(uint256 _amount) public payable {
-        uint _previousBalance = balance;
-
-        // make sure this is the owner
-        require(msg.sender == owner, "You are not the owner of this account");
-
-        // perform transaction
-        balance += _amount;
-
-        // assert transaction completed successfully
-        assert(balance == _previousBalance + _amount);
-
-        // emit the event
-        emit Deposit(_amount);
+    function transfer(address _from, address _to, uint _number) public payable{
+        require(balances[_from]>= _number,"Not enough funds");
+        balances[_from] -= _number;
+        balances[_to] += _number;
+        emit Transfer(_from, _to, _number);
     }
 
-    // custom error
-    error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
-
-    function withdraw(uint256 _withdrawAmount) public {
-        require(msg.sender == owner, "You are not the owner of this account");
-        uint _previousBalance = balance;
-        if (balance < _withdrawAmount) {
-            revert InsufficientBalance({
-                balance: balance,
-                withdrawAmount: _withdrawAmount
-            });
-        }
-
-        // withdraw the given amount
-        balance -= _withdrawAmount;
-
-        // assert the balance is correct
-        assert(balance == (_previousBalance - _withdrawAmount));
-
-        // emit the event
-        emit Withdraw(_withdrawAmount);
+    function getBalance(address _account)public view returns(uint){
+        return  balances[_account];
     }
 
-    function checkOwner()public pure returns(string memory){
-        string memory name="Moshahid Raza";
-        return name;
-    }
-
-    
 }
